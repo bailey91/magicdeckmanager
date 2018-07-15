90c73ec42cb8d5cc490bf6ad17e5124fabada255
Tom Bailey <tom.bailey17@gmail.com>
2018-07-15 17:39:06 +0100

    Added SDK

---
+++ MagicDeckManager/Models/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicDeckManager.Models
+{
+    public class Card
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string manaCost { get; set; }
+        public int cmc { get; set; }
+        public List<string> colors { get; set; }
+        public List<string> colorIdentity { get; set; }
+        public string type { get; set; }
+        public string rarity { get; set; }
+        public string set { get; set; }
+        public string setName { get; set; }
+        public string imageUrl { get; set; }
+    }
+}
